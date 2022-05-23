d0d3b01881fb71762d324a5954ec4efa6d68a6fb
shchegrikovich-palta <sergey.shchegrikovich@palta.com>
2022-05-23 15:17:47 +0300

    Fixed payments

--- src/components/PaymentScreen.tsx
+++ src/components/PaymentScreen.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import "@primer-io/checkout-web/dist/Checkout.css";
-import { createPaymentClient, PaymentClient } from "@palta-brain/payments";
+import {
+  createPaymentClient,
+  PaymentClient,
+  PricePoint,
+} from "@palta-brain/payments";
 
 const customerId = {
   type: "merchant-uuid",
@@ -9,6 +13,9 @@ const customerId = {
 };
 
 export const PaymentScreen = (): ReactElement => {
+  const [pricePoints, setPricePoints] = useState<PricePoint[]>([]);
+  const [client, setClient] = useState<null | PaymentClient>(null);
+
   const loadCheckout = async () => {
     const metadata: Record<string, any> = {};
 
@@ -25,20 +32,52 @@ export const PaymentScreen = (): ReactElement => {
     };
 
     const client: PaymentClient = createPaymentClient(settings);
+    setClient(client);
     const pricePoints = await client.getPricePoints();
-    await client.renderPaymentForm({
-      containerId: "#checkout-container",
-      sku: pricePoints[0].sku,
-    });
+    setPricePoints(pricePoints);
   };
 
   useEffect(() => {
     loadCheckout().then(() => {});
   }, []);
 
+  const buy = (sku: string) => {
+    return () => {
+      client.renderPaymentForm({
+        containerId: "#checkout-container",
+        sku: sku,
+      });
+    };
+  };
+
   return (
     <>
       Payment Screen
+      <table>
+        <tr>
+          <td>SKU & Subscription type</td>
+          <td>Intro price</td>
+          <td>Subscription price</td>
+        </tr>
+        {pricePoints.map(function (pricePoint, i) {
+          return (
+            <tr>
+              <td>
+                {pricePoint.sku} - {pricePoint.subscriptionType}
+              </td>
+              <td>
+                {pricePoint.introTotalPrice} {pricePoint.currencyCode}
+              </td>
+              <td>
+                {pricePoint.subscriptionTotalPrice} {pricePoint.currencyCode}
+              </td>
+              <td>
+                <button onClick={buy(pricePoint.sku)}>Buy</button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
       <br />
       <div id={"checkout-container"}></div>
     </>
